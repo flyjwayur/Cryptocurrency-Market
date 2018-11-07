d14398f5ac0fe53fc8369d6a20f0a15b6e4b2793
Hye Soo Park <flyjwayur@gmail.com>
2018-11-07 14:52:35 +0200

    Fetch the data from API

--- src/App.js
+++ src/App.js
@@ -2,9 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    coins: null
+  }
+
+  componentDidMount () {
+    const url = 'https://api.coinmarketcap.com/v1/ticker/?limit=2000';
+    fetch (url).then (response => response.json()).then (coins => {
+      this.setState({
+        coins
+      });
+    });
+  }
+  
+  componentDidUpdate(){
+    console.log('coins', this.state.coins);
+  }
+
   render() {
     return (
       <div className="App">
+
       </div>
     );
   }
