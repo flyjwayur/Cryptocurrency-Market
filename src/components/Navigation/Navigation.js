cd3f708166da002a39bf1fcd6b7c508a071b677b
Hye Soo Park <flyjwayur@gmail.com>
2018-11-20 18:31:39 +0200

    Fix nav : Widen the nav and delete unused props

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import classes from './navigation.module.css'
 
-const Navigation = ({sortOrder, handleSortInput}) => {
+const Navigation = () => {
   return (
     <div className={classes.navWrapper}>
       <h1 className={classes.mainTitle}>CryptoMarket</h1>
